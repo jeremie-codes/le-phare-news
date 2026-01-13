480a4cf0bbb4e916db9b370a1bfb5dfb16828697
jeremie-codes <jrmmianda@gmail.com>
2026-01-13 12:32:45 +0100

    update

--- app/Filament/Resources/BannerResource.php
+++ app/Filament/Resources/BannerResource.php
@@ -17,6 +17,7 @@ use Filament\Tables\Columns\BooleanColumn;
 use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
--- app/Filament/Resources/EnVedetteResource.php
+++ app/Filament/Resources/EnVedetteResource.php
@@ -10,6 +10,7 @@ use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
