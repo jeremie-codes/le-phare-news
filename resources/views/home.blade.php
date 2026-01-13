f977fb4156b96cd9526985b367f2e31f9161f57d
jeremie-codes <jrmmianda@gmail.com>
2026-01-13 12:41:39 +0100

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,7 @@
                     @if(count($banners) > 0)
                         @foreach($banners as $banner)
                         <div class="overflow-hidden position-relative" style="height: 500px;">
-                            <img class="img-fluid h-100" src="{{ asset( $banner->cover_image ? $banner->cover_image: 'assets/img/bannerNews-1.jpg') }}" style="object-fit: cover;">
+                            <img class="img-fluid h-100" src="{{ asset( $banner->cover_image ? 'storage/' . $banner->cover_image: 'assets/img/bannerNews-1.jpg') }}" style="object-fit: cover;">
                             <div class="overlay">
                                 <a href="{{ route('news.show', $banner->id) }}" class="m-0 text-white h2 text-uppercase font-weight-bold" href="#">Lorem ipsum dolor sit amet elit. Proin vitae porta diam...</a>
                             </div>
